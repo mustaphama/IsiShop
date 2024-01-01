86d8ec5195b0234d1eebb554b2de40fbd40cacb8
salim2502 <chabchoubsalim25@gmail.com>
2024-01-01 01:49:42 +0100

    normalement tout marche parfaitement meme en se connectant

--- Modele/Modele.php
+++ Modele/Modele.php
@@ -47,6 +47,23 @@ class ModeleWeb4Shop {
             $loginStmt->bindParam(':username', $username);
             $loginStmt->bindParam(':password', $hashedPassword);
             $loginStmt->execute();
+            $addinsert =$this->connexion->prepare("INSERT INTO delivery_addresses (firstname, lastname, add1,add2,city,postcode,phone,email) VALUES (:forname,:surname,:add1,:add2,:add3,:postcode,:phone,:email)");
+            $addinsert->bindParam(':forname', $forname);
+            $addinsert->bindParam(':surname', $surname);
+            $addinsert->bindParam(':add1', $add1);
+            $addinsert->bindParam(':add2', $add2);
+            $addinsert->bindParam(':add3', $add3);
+            $addinsert->bindParam(':postcode', $postcode);
+            $addinsert->bindParam(':phone', $phone);
+            $addinsert->bindParam(':email', $email);
+            $addinsert->execute();
+            $lastadd = $this->connexion->lastInsertId();
+            $sessionid = session_id();
+            $ordersins =$this->connexion->prepare("INSERT INTO orders (customer_id,registered,delivery_add_id,payment_type, date , status , session,total) VALUES (:customer_id,1,:delivery_add_id,NULL,NULL,0,:session ,0)");
+            $ordersins->bindParam(':customer_id', $customerId);
+            $ordersins->bindParam(':delivery_add_id', $lastadd);
+            $ordersins->bindParam(':session', $sessionid);
+            $ordersins->execute();
             $this->connexion->commit();
 
             return true;
@@ -56,6 +73,54 @@ class ModeleWeb4Shop {
             return false;
         }
     }
+
+    public function ajout_produit($orderid,$productid,$quantity){
+        $this->connexion->beginTransaction();
+        try {
+            $productadd = $this->connexion->prepare("INSERT INTO orderitems (order_id,product_id,quantity) VALUES (:order_id,:product_id,:quantity)");
+            $productadd->bindParam(':order_id', $orderid);
+            $productadd->bindParam(':product_id',$productid);
+            $productadd->bindParam(':quantity',$quantity);
+            $productadd->execute();
+            $this->connexion->commit();
+            return true;
+        } catch (PDOException $e) {
+            $this->connexion->rollBack();
+            echo "Échec de la requête SQL  d'ajout : " . $e->getMessage();
+            return false;
+        }
+    }
+
+    public function supprime_produit($idproduit){
+        $this->connexion->beginTransaction();
+        try {
+            $suppression = $this->connexion->prepare("DELETE FROM orderitems WHERE product_id = :product");
+            $suppression->bindParam(':product',$idproduit);
+            $suppression->execute();
+            $this->connexion->commit();
+            return true;
+        }catch (PDOException $e) {
+            $this->connexion->rollBack();
+            echo "Échec de la requête SQL  de suppression : " . $e->getMessage();
+            return false;
+        }
+    }
+
+    public function augmente_quantity($quantity, $idproduit,$orderid){
+        try{
+            $augmentation = $this->connexion->prepare("UPDATE orderitems SET quantity = :quantity WHERE order_id = :order_id AND product_id = :product_id");
+            $augmentation->bindParam(':quantity', $quantity, PDO::PARAM_INT);
+            $augmentation->bindParam(':order_id', $orderid, PDO::PARAM_INT);
+            $augmentation->bindParam(':product_id', $idproduit, PDO::PARAM_INT);
+            $augmentation->execute();
+            return true;
+        }catch (PDOException $e) {
+            $this->connexion->rollBack();
+            echo "Échec de la requête SQL  de modification de quantite : " . $e->getMessage();
+            return false;
+        }
+        
+    }   
 
     public function fermerConnexion() {
         $this->connexion = null;
